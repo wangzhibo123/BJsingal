2a7574b190a40a9aeec369d0b6f87eb22b5ae4a6
zhangchaohui <15001300136@163.com>
2020-09-28 19:26:45 +0800

    update

--- src/router.js
+++ src/router.js
@@ -9,22 +9,24 @@ import loadable from 'loadable-components'
 
 import LoadingPage from './container/LoadingPage/LoadingPage'
 
+const Login = loadable(() => import('./container/Login/Login'), { LoadingComponent: LoadingPage })
+const Header = loadable(() => import('./container/Header/Header'), { LoadingComponent: LoadingPage })
 const HomePage = loadable(() => import('./container/HomePage/HomePage'), { LoadingComponent: LoadingPage })
 const PlancontrolPage = loadable(() => import('./container/PlancontrolPage/PlancontrolPage'), { LoadingComponent: LoadingPage })
-const Header = loadable(() => import('./container/Header/Header'), { LoadingComponent: LoadingPage })
 
 const Parent = () => (
   <React.Fragment>
     <Route path="*" component={Header} />
     <Route exact path="/home" component={HomePage} />
     <Route exact path="/plancontrolpage" component={PlancontrolPage} />
-    <Redirect exact from="/" to="/home" />
   </React.Fragment>
 )
 export default function BasicRouter() {
   return (
     <Router>
       <Switch>
+        <Redirect exact from="/" to="login" />
+        <Route exact path="/login" component={Login} />
         <Route path="/" component={Parent} />
       </Switch>
     </Router>
