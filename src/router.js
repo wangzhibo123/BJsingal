5293c6c4030fef179f3f2b2c4b81d128cf4fa7a5
wangzhibo123 <5715741+wangzhibo123@user.noreply.gitee.com>
2020-10-09 18:33:53 +0800

    update

--- src/router.js
+++ src/router.js
@@ -14,11 +14,14 @@ const Header = loadable(() => import('./container/Header/Header'), { LoadingComp
 const HomePage = loadable(() => import('./container/HomePage/HomePage'), { LoadingComponent: LoadingPage })
 const PlancontrolPage = loadable(() => import('./container/PlancontrolPage/PlancontrolPage'), { LoadingComponent: LoadingPage })
 const InterMonitor = loadable(() => import('./container/InterMonitor/InterMonitor'), { LoadingComponent: LoadingPage })
-
+const LineMon = loadable(() => import("./container/CenterControl/MainMonitoring/MainMonitoring"), { LoadingComponent: LoadingPage })
+const AreaMon = loadable(() => import("./container/CenterControl/AreaMonitoring/AreaMonitoring"), { LoadingComponent: LoadingPage })
 const Parent = () => (
   <React.Fragment>
     <Route path="*" component={Header} />
     <Route exact path="/home" component={HomePage} />
+    <Route path="/lineMon" component={LineMon} />
+    <Route path="/areaMon" component={AreaMon} />
     <Route exact path="/plancontrolpage" component={PlancontrolPage} />
     <Route exact path="/intermonitor" component={InterMonitor} />
   </React.Fragment>
