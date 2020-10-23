2acbf27eaf82174af3d2fc71eadbd3d37ee8ef99
wangzhibo123 <2501212369@qq.com>
2020-10-23 18:08:36 +0800

    update

---
+++ src/container/CenterControl/AreaMonitoring/AreaMessage/AreaMessage.jsx
@@ -0,0 +1,121 @@
+import React, { Component } from 'react'
+import styles from './AreaMessage.module.scss'
+class TimingMessage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+  componentDidMount() {
+  }
+  render() {
+    return (
+      <div className={styles.mountingTable}>
+        <div className={styles.mountingThead}>
+          <div className={styles.mountingTh}>路口号</div>
+          <div className={styles.mountingTh}>控制模式</div>
+          <div className={styles.mountingTh}>当前方案</div>
+          <div className={styles.mountingTh}>周期</div>
+          <div className={styles.mountingTh}>相位差</div>
+          <div className={styles.mountingTh}>当前阶段</div>
+          <div className={styles.mountingTh}>时间(秒)</div>
+        </div>
+        <div className={styles.mountingTbody}>
+          <div className={styles.mountingTr}>
+            <div className={styles.mountingTd}>25521-郭沫若故居</div>
+            <div className={styles.mountingTd}>中心控制</div>
+            <div className={styles.mountingTd}>4</div>
+            <div className={styles.mountingTd}>141</div>
+            <div className={styles.mountingTd}>0</div>
+            <div className={styles.mountingTd}>1-东西左直</div>
+            <div className={styles.mountingTd}>73</div>
+          </div>
+          <div className={styles.mountingTr}>
+            <div className={styles.mountingTd}>25521-郭沫若故居</div>
+            <div className={styles.mountingTd}>中心控制</div>
+            <div className={styles.mountingTd}>4</div>
+            <div className={styles.mountingTd}>141</div>
+            <div className={styles.mountingTd}>0</div>
+            <div className={styles.mountingTd}>1-东西左直</div>
+            <div className={styles.mountingTd}>73</div>
+          </div>
+          <div className={styles.mountingTr}>
+            <div className={styles.mountingTd}>25521-郭沫若故居</div>
+            <div className={styles.mountingTd}>中心控制</div>
+            <div className={styles.mountingTd}>4</div>
+            <div className={styles.mountingTd}>141</div>
+            <div className={styles.mountingTd}>0</div>
+            <div className={styles.mountingTd}>1-东西左直</div>
+            <div className={styles.mountingTd}>73</div>
+          </div>
+          <div className={styles.mountingTr}>
+            <div className={styles.mountingTd}>25521-郭沫若故居</div>
+            <div className={styles.mountingTd}>中心控制</div>
+            <div className={styles.mountingTd}>4</div>
+            <div className={styles.mountingTd}>141</div>
+            <div className={styles.mountingTd}>0</div>
+            <div className={styles.mountingTd}>1-东西左直</div>
+            <div className={styles.mountingTd}>73</div>
+          </div>
+          <div className={styles.mountingTr}>
+            <div className={styles.mountingTd}>25521-郭沫若故居</div>
+            <div className={styles.mountingTd}>中心控制</div>
+            <div className={styles.mountingTd}>4</div>
+            <div className={styles.mountingTd}>141</div>
+            <div className={styles.mountingTd}>0</div>
+            <div className={styles.mountingTd}>1-东西左直</div>
+            <div className={styles.mountingTd}>73</div>
+          </div>
+          <div className={styles.mountingTr}>
+            <div className={styles.mountingTd}>25521-郭沫若故居</div>
+            <div className={styles.mountingTd}>中心控制</div>
+            <div className={styles.mountingTd}>4</div>
+            <div className={styles.mountingTd}>141</div>
+            <div className={styles.mountingTd}>0</div>
+            <div className={styles.mountingTd}>1-东西左直</div>
+            <div className={styles.mountingTd}>73</div>
+          </div>
+          <div className={styles.mountingTr}>
+            <div className={styles.mountingTd}>25521-郭沫若故居</div>
+            <div className={styles.mountingTd}>中心控制</div>
+            <div className={styles.mountingTd}>4</div>
+            <div className={styles.mountingTd}>141</div>
+            <div className={styles.mountingTd}>0</div>
+            <div className={styles.mountingTd}>1-东西左直</div>
+            <div className={styles.mountingTd}>73</div>
+          </div>
+          <div className={styles.mountingTr}>
+            <div className={styles.mountingTd}>25521-郭沫若故居</div>
+            <div className={styles.mountingTd}>中心控制</div>
+            <div className={styles.mountingTd}>4</div>
+            <div className={styles.mountingTd}>141</div>
+            <div className={styles.mountingTd}>0</div>
+            <div className={styles.mountingTd}>1-东西左直</div>
+            <div className={styles.mountingTd}>73</div>
+          </div>
+          <div className={styles.mountingTr}>
+            <div className={styles.mountingTd}>25521-郭沫若故居</div>
+            <div className={styles.mountingTd}>中心控制</div>
+            <div className={styles.mountingTd}>4</div>
+            <div className={styles.mountingTd}>141</div>
+            <div className={styles.mountingTd}>0</div>
+            <div className={styles.mountingTd}>1-东西左直</div>
+            <div className={styles.mountingTd}>73</div>
+          </div>
+          <div className={styles.mountingTr}>
+            <div className={styles.mountingTd}>25521-郭沫若故居</div>
+            <div className={styles.mountingTd}>中心控制</div>
+            <div className={styles.mountingTd}>4</div>
+            <div className={styles.mountingTd}>141</div>
+            <div className={styles.mountingTd}>0</div>
+            <div className={styles.mountingTd}>1-东西左直</div>
+            <div className={styles.mountingTd}>73</div>
+          </div>
+        </div>
+        
+      </div>
+    )
+  }
+}
+
+export default TimingMessage
