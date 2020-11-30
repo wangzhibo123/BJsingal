f31472a2add8cc0a1357cb0cd9688a6a7691c040
wangzhibo123 <2501212369@qq.com>
2020-11-30 16:32:47 +0800

    update

--- src/container/utils/getInterfaceData.js
+++ src/container/utils/getInterfaceData.js
@@ -3,7 +3,11 @@ import axios from 'axios'
 const getInterfaceData = () => {
     const instance = axios.create({
 <<<<<<< HEAD
+<<<<<<< HEAD
         // baseURL: 'http://192.168.1.124:20191',
+=======
+        // baseURL: 'http://39.100.128.220:12345',
+>>>>>>> 097117c6f8eb33c8ca201d93302aad68daeb66df
 =======
         // baseURL: 'http://39.100.128.220:12345',
 >>>>>>> 097117c6f8eb33c8ca201d93302aad68daeb66df
