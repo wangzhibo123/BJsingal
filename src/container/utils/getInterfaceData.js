c7123bf96fdd96250929811a11d5489b8f702a59
wangzhibo123 <2501212369@qq.com>
2020-11-30 16:40:44 +0800

    update

--- src/container/utils/getInterfaceData.js
+++ src/container/utils/getInterfaceData.js
@@ -6,6 +6,7 @@ const getInterfaceData = () => {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         // baseURL: 'http://192.168.1.124:20191',
 =======
         // baseURL: 'http://39.100.128.220:12345',
@@ -19,6 +20,9 @@ const getInterfaceData = () => {
 =======
         // baseURL: 'http://39.100.128.220:12345',
 >>>>>>> 097117c6f8eb33c8ca201d93302aad68daeb66df
+=======
+        // baseURL: 'http://39.100.128.220:12345',
+>>>>>>> 67a756b5171ca085f77d8bcf712ae49d0bc2c56c
         // baseURL: 'http://11.82.12.118:20191'
     })
     return instance
