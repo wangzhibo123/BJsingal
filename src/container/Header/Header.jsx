f8b1a26fc9d3870250bba51b264ddf70701da0e4
zhangchaohui <15001300136@163.com>
2020-12-03 10:34:10 +0800

    update

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -18,7 +18,7 @@ class Header extends Component {
       { name: '预案控制', id: '0-3', path: '/plancontrolpage', children: [] },
       {
         name: '信号优化', id: '0-4',
-        children: [{ name: '局信号优化', id: '1-4', path: 'office' }, { name: '区信号优化', id: '2-4', path: 'districtOpt' }, { name: '路口优化', id: '3-4', path: 'interOpt' }, { name: '区域优化', id: '4-4', path: 'regionOpt' }]
+        children: [{ name: '局信号优化', id: '1-4', path: '/office' }, { name: '区信号优化', id: '2-4', path: '/districtOpt' }, { name: '路口优化', id: '3-4', path: '/interOpt' }, { name: '区域优化', id: '4-4', path: '/regionOpt' }]
       },
       { name: '效果评价', id: '0-5', children: [{ name: '路口评价', id: '1-5', path: '/inter' }, { name: '干线评价', id: '2-5', path: '/artery' }, { name: '区域评价', id: '3-5', path: '/area' }] },
       {
--- src/container/InterMonitor/InterConfMsg/SingalParams/PhaseMsg/PhaseMsg.jsx
+++ src/container/InterMonitor/InterConfMsg/SingalParams/PhaseMsg/PhaseMsg.jsx
@@ -1,22 +1,58 @@
 import React, { Component } from 'react'
 import { Select } from 'antd'
 import phasePic from '../../../../imgs/01.png'
-
+import axiosInstance from "../../../../utils/getInterfaceData"
 const { Option } = Select
 class PhaseMsg extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      interInfoList: null,
+      interDirList: null,
+      interMoveList: null,
+    }
+    this.dirInfoList="/control-application-front/basic/info/listCodeByCodeType"
+    this.dirPhaseList="/control-application-front/signal/config/phase/listPhaseInfo"
   }
-  componentDidMount = () => { }
+  componentDidMount = () => {
+    this.getInterDirInfoList(6)
+    this.getInterDirPhaseList(1000084)
+  }
+  getInterDirInfoList=(type)=>{
+    axiosInstance.get(`${this.dirInfoList}?codeType=${type}`).then((res) => {
+      const { code , data} = res.data
+      if (code === 200 && data) {
+        const stateName = type === 6 ? 'interInfoList' : 'interMoveList'
+        this.setState({ [stateName]: data })
+      }
+    })
+  }
+  getInterDirPhaseList = (type) => {
+    axiosInstance.get(`${this.dirPhaseList}?unitId=${type}`).then((res) => {
+      const { code, data } = res.data
+      if (code === 200 && data) {
+        console.log(data,"data")
+        const stateName = type === 1000084 ? 'interDirList' : 'interMoveList'
+        this.setState({ [stateName]: data })
+      }
+    })
+  }
+  getInterDirInfoListProperty=(code)=>{
+    this.state.interInfoList.find((item)=>{
+      if(item&&item.cCode===code){
+        console.log(item,"dasdasdw")
+      }
+    })
+  }
   render() {
+    const { interDirList } =this.state;
     return (
       <div className="paramsTable">
         <span className="addBtn">新增相位</span>
         <div className="paramsThead">
           <div className="paramsTh">相位序号</div>
           <div className="paramsTh">相位名称</div>
-          <div className="paramsTh">相位特征</div>
+          {/* <div className="paramsTh">相位特征</div> */}
           <div className="paramsTh">相位标识</div>
           <div className="paramsTh">放行人行道方向</div>
           <div className="paramsTh">最大率</div>
@@ -27,31 +63,38 @@ class PhaseMsg extends Component {
           <div className="paramsTh">操作</div>
         </div>
         <div className="paramsTbody">
+          {
+            interDirList&&
+            interDirList.map(item=>{
+              return (
+                <div className="paramsTr" key={item.id}>
+                    <div className="paramsTd">{item.phaseno}</div>
+                    <div className="paramsTd">{item.phasename}</div>
+                    {/* <div className="paramsTd">{item.feature===1 ?<span>关键</span> : <span>非关键</span>}</div> */}
+                    <div className="paramsTd"><img src={phasePic} width="35px" height="35px" alt="" /></div>
+                    <div className="paramsTd">东向西{console.log(item.peddirlist)}</div>
+                    <div className="paramsTd">{item.greenmax}</div>
+                    <div className="paramsTd">{item.greenmin}</div>
+                    <div className="paramsTd">{item.redyellow}</div>
+                    <div className="paramsTd">{item.yellow}</div>
+                    <div className="paramsTd">{item.allred}</div>
+                    <div className="paramsTd">
+                      <span className="editBtn">编辑</span>
+                      <span className="editBtn">删除</span>
+                    </div>
+                </div>
+              )
+            })
+          }
           <div className="paramsTr">
             <div className="paramsTd">1</div>
             <div className="paramsTd">A</div>
-            <div className="paramsTd">非关键</div>
-            <div className="paramsTd"><img src={phasePic} width="35px" height="35px" alt="" /></div>
-            <div className="paramsTd">东向西</div>
-            <div className="paramsTd">40</div>
-            <div className="paramsTd">10</div>
-            <div className="paramsTd">5</div>
-            <div className="paramsTd">2</div>
-            <div className="paramsTd">10</div>
-            <div className="paramsTd">
-              <span className="editBtn">编辑</span>
-              <span className="editBtn">删除</span>
-            </div>
-          </div>
-          <div className="paramsTr">
-            <div className="paramsTd">1</div>
-            <div className="paramsTd">A</div>
-            <div className="paramsTd">
+            {/* <div className="paramsTd">
               <Select defaultValue="0">
                 <Option key="0" vlaue="0">请选择</Option>
                 <Option key="1" vlaue="1">1</Option>
               </Select>
-            </div>
+            </div> */}
             <div className="paramsTd">
               <Select defaultValue="0">
                 <Option key="0" vlaue="0">请选择</Option>
