f0b0097560ed1f91e7e69ea886f2e481f7e6f3b5
wangzhibo123 <2501212369@qq.com>
2020-12-03 18:23:55 +0800

    update

--- src/container/utils/video/video.jsx
+++ src/container/utils/video/video.jsx
@@ -5,7 +5,6 @@ import React, { Component } from 'react'
 import 'video.js/dist/video-js.css'
 import 'videojs-flash'
 import videojs from 'video.js'
-import { Button } from "antd"
 
 
 class VideoApp extends Component{
@@ -13,7 +12,6 @@ class VideoApp extends Component{
     super(props)
     this.state={
       nowPlay:"",
-      showButton:this.props.showB && true,
       width: this.props.width || "483px",
       height: this.props.height || "300px",
       newState:this.props.arlVideo
@@ -59,42 +57,28 @@ class VideoApp extends Component{
           this.player.play();
   }
   componentWillUnmount = () =>{
-    const {newState} =this.state;
+    const { newState} =this.state;
     const myVideoElem = document.getElementById(newState[0].id.slice(0,6));
     if(myVideoElem){
         const player = videojs(newState[0].id.slice(0,6));
         player.dispose();
     }
-  }
+  } 
   render(){
-      let li = {
-      }
-      let playing = {
-         color:"#fff"
-      }
+      const { newState, width ,height} =this.state;
     return(
        <div className="VideoAppBox">
               <div>
-                  <ul>
                   {
-                    this.state.showButton&&this.state.newState.map((item,index)=>{
-                          return <Button  key={item.name} onClick={()=>this.handleClick(item)} style={{width:"66px",height:"40px",background: "#010F46",border: "1px solid #4289E1",color: "#515A8B",fontSize: "20px",boxShadow:"inset 0px 0px 1.7px 1px #4289E1"}}>
-                                      <span style={this.state.displayStyle === item.displayStyle ? playing : li }>{item.name}</span>
-                                  </Button>
-                      })
-                  }
-                  </ul>
-                  {
-                    this.state.newState.map((item,index)=>{
-                      return <video key={index} style={{width:this.state.width,height:this.state.height,borderRadius:"6px",textAlign:"center",marginTop:"15px",marginLeft:'15px'}} id={item.id.slice(0,6)} className="video-js vjs-default-skin">
+                    newState.map((item,index)=>{
+                      return <video key={index} style={{width:width,height:height,borderRadius:"6px",textAlign:"center",marginLeft:'15px'}} id={item.id.slice(0,6)} className="video-js vjs-default-skin">
                       </video>
                     })
                   }
-                  
               </div>
         </div>
       )
-  }
+    }
 }
 
 export default VideoApp
